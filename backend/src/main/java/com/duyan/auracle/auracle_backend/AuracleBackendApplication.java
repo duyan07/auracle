b258321eb1e0d522524e42dfd02290ab581a917d
nguyean <nguyean@uw.edu>
2025-10-08 01:23:21 -0700

    next and springboot

---
+++ backend/src/main/java/com/duyan/auracle/auracle_backend/AuracleBackendApplication.java
@@ -0,0 +1,13 @@
+package com.duyan.auracle.auracle_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuracleBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuracleBackendApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/duyan/auracle/auracle_backend/AuracleBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.duyan.auracle.auracle_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuracleBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
